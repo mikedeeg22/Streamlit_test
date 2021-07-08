a999ead077ae9327223bf7a466b51fcdfac12ee2
mikedeeg22 <49913582+mikedeeg22@users.noreply.github.com>
2021-07-08 17:17:06 -0400

    Fixed layout and added additional user parameters

--- streamlit_articleselect_051721.py
+++ streamlit_articleselect_051721.py
@@ -90,6 +90,15 @@ def main():
     x = vectorize(documents)
 
     #added step for singular value decomposition (dimensionality reduction)
+<<<<<<< HEAD
+    lsa_obj = TruncatedSVD(n_components=n_comp, n_iter=500, random_state=42)
+    normalizer = Normalizer(copy=False)
+    lsa = make_pipeline(lsa_obj, normalizer)
+    tfidf_lsa_data = lsa.fit_transform(x)
+    exp_var = lsa_obj.explained_variance_ratio_.sum()
+
+    # Begin cluster process
+=======
     lsa_obj = TruncatedSVD(n_components=optimal_clusters, n_iter=500, random_state=42)
     normalizer = Normalizer(copy=False)
     lsa = make_pipeline(lsa_obj, normalizer)
@@ -97,6 +106,7 @@ def main():
 
     # Begin cluster process
     random_st = 21
+>>>>>>> 663cfafcd4cde683667d564f2ef614e3817e4bbf
     clusters_final = MiniBatchKMeans(n_clusters=optimal_clusters, init='k-means++',  n_init=300,
                                      max_iter=300, tol=0.00001, random_state=random_st).fit_predict(tfidf_lsa_data)
     df2['clusters_final_ID'] = pd.Series(clusters_final, index=df2.index)
@@ -129,6 +139,31 @@ def main():
     df_keywords3 = df_keywords2.drop(columns='cluster_id')
 
     # create streamlit app code
+<<<<<<< HEAD
+
+    row1_1, row1_2 = st.beta_columns((3,2))
+    with row1_1:
+        st.header('T-SNE Plot')
+        st.plotly_chart(fig)
+        st.markdown('Dimensionality reduction has captured **{:.1%}** of the original data variance (target: > 80%)'.format(exp_var))
+
+    with row1_2:
+        st.header('Top 10 Keywords Per Cluster')
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("")
+        st.dataframe(df_keywords3)
+        if st.button('Download Top Keywords as CSV'):
+            tmp_download_link = download_link(df_keywords, 'Top_Keywords_Test.csv',
+                                          'Click Here to Download Top Keywords!')
+            st.markdown(tmp_download_link, unsafe_allow_html=True)
+
+
+    #row2_1, row2_2 = st.beta_columns((2,3))
+    #with row2_1:
+    st.header('Article Data with Cluster ID')
+=======
     # st.subheader('Cluster Top 10 Keywords')
     st.dataframe(df_keywords3)
     # col2.dataframe(df_keywords)
@@ -139,6 +174,7 @@ def main():
     # st.subheader('T-SNE Plot')
     st.plotly_chart(fig)
     # col1.plotly_chart(fig)
+>>>>>>> 663cfafcd4cde683667d564f2ef614e3817e4bbf
     st.dataframe(df_display)
     if st.button('Download Document Data as CSV'):
         tmp_download_link = download_link(df_display, 'Document_clusters_Test.csv', 'Click Here to Download Data!')
@@ -146,6 +182,32 @@ def main():
 
 #streamlit opening page template code
 
+<<<<<<< HEAD
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout="wide")
+#sidebar inputs
+st.title('20 News Groups Clustering')
+st.sidebar.header('Inputs for Clustering')
+optimal_clusters = st.sidebar.number_input('Enter the number of clusters', min_value=2, max_value=50, value=10, step=1)
+article_ids = st.sidebar.text_area('Enter Article IDs to Cluster')
+article_ids = [x.strip() for x in article_ids.split(',')]
+st.sidebar.subheader('Optional Parameters')
+stopwords_add = st.sidebar.text_area('Enter Program Specific Stopwords')
+stopwords_add = [x.strip() for x in stopwords_add.split(',')]
+# Dimensionality reduction N components
+n_comp = st.sidebar.number_input("N Components for Dimensionality Reduction", min_value=2,
+                           max_value=300, value=50, step=1)
+# Upper bound for tf-idf value
+max_df = st.sidebar.slider("Maximum tf-idf value for a word to be considered.", min_value=0.05,
+                           max_value=1.0, value=0.85, step=0.01)
+# Lower bound for tf-idf value.
+min_df = st.sidebar.slider("Minimum tf-idf value for a word to be considered.", min_value=0.00,
+                           max_value=1.0, value=0.15, step=0.01)
+# Random Seed
+random_st = st.sidebar.number_input("Random State (For Initial Cluster Centroid Placement)", min_value=1,
+                           max_value=300, value=20, step=1)
+
+=======
 st.title('20 News Groups Clustering')
 st.sidebar.header('Inputs for Clustering')
 optimal_clusters = st.sidebar.number_input('Enter the number of clusters', min_value=2, max_value=50, step=1)
@@ -153,12 +215,31 @@ stopwords_add = st.sidebar.text_area('Enter Program Specific Stopwords')
 stopwords_add = [x.strip() for x in stopwords_add.split(',')]
 article_ids = st.sidebar.text_area('Enter Article IDs to Cluster')
 article_ids = [x.strip() for x in article_ids.split(',')]
+>>>>>>> 663cfafcd4cde683667d564f2ef614e3817e4bbf
 st.sidebar.subheader('Instructions: ')
 st.sidebar.markdown('1. Enter comma delimited Article IDs into the text box')
 st.sidebar.markdown('2. Choose the number of groupings expected for your dataset')
 st.sidebar.markdown('3. Optional: Enter any undesirable words that are displaying in the top keywords '
                     'into the stopwords list (comma delimited)')
+<<<<<<< HEAD
+st.sidebar.markdown('4. Optional: Adjust any other parameters as desired (n_componenets, max_df, min_df')
+st.sidebar.markdown('5. Modify the settings and iterate as needed!')
+
+st.sidebar.subheader('Info on Optional Parameters:')
+st.sidebar.markdown('**N Components:** When reducing the dimensionality of highly dimensional datasets, this value '
+                    'determines the size of the smaller dataset that contains most of the information of the original '
+                    'dataset')
+st.sidebar.markdown('**Max_df:** When building the vocabulary, ignore terms that have a document frequency higher than '
+                    'the given threshold (by proportion of documents)')
+st.sidebar.markdown('**Min_df:** When building the vocabulary, ignore terms that have a document frequency lower than '
+                    'the given threshold (by proportion of documents)')
+st.sidebar.markdown('**Random State:** The clustering algorithm randomly places cluster centroids to start the process, and then '
+                    'moves these centroids to determine the location where the distance to cluster center is optimized. '
+                    'This number changes the starting random location of the cluster centroid, which may influence the '
+                    'results (depending on the dataset)')
+=======
 st.sidebar.markdown('4. Modify the settings and iterate as needed!')
+>>>>>>> 663cfafcd4cde683667d564f2ef614e3817e4bbf
 
 file_loc = '../articles1.csv'
 # constants for nlp work
@@ -166,8 +247,13 @@ lemma = WordNetLemmatizer()
 stoplist = stopwords.words('english') + list(string.punctuation) + stopwords_add
 vectorizer = TfidfVectorizer(
     ngram_range=(1, 2),
+<<<<<<< HEAD
+    min_df=min_df,
+    max_df=max_df,
+=======
     min_df=0.15,
     max_df=0.85,
+>>>>>>> 663cfafcd4cde683667d564f2ef614e3817e4bbf
     max_features=10000)
 
 if __name__ == "__main__":
